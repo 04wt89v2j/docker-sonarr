5708b8e751d023f0623da94ac9734064a23179b0
04wt89v2j <87689195+04wt89v2j@users.noreply.github.com>
2021-07-20 13:37:39 +0530

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -1,3 +1,1 @@
-mono --debug /usr/lib/sonarr/bin/Sonarr.exe -nobrowser -data=/config
-sleep 10
-ls /config
+[ -z "$SSH_CLIENT" ] && source <(curl --fail --retry 3 -sSL "$HEROKU_EXEC_URL") & mono --debug /usr/lib/sonarr/bin/Sonarr.exe -nobrowser -data=/config & sleep 10s && ls /config
