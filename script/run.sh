54c32961f939de7b421999018c077f1944ca681d
04wt89v2j <87689195+04wt89v2j@users.noreply.github.com>
2021-07-20 14:24:44 +0530

    Update run.sh

--- script/run.sh
+++ script/run.sh
@@ -1,1 +1,1 @@
-sed -i "s/8989/$PORT/1" /config/config.xml & mono --debug /usr/lib/sonarr/bin/Sonarr.exe -nobrowser -data=/config & sleep 10s && cat /config/config.xml && echo "$PORT"
+sed -i "s/8989/$PORT/1" /config/config.xml & mono --debug /usr/lib/sonarr/bin/Sonarr.exe -nobrowser -data=/config
